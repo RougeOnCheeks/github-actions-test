b7c6f982578921c996b36293d69aa6ec2e8e888e
RougeOnCheeks <liliana@kakao.com>
2023-01-13 16:23:05 +0900

    deploy.sh 작성 backend push main #13

---
+++ backend/deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+echo "> BE 배포"
+today=$(date "+%Y-%m-%d %H:%M")
+sudo cp /usr/local/lib/apache-tomcat-8.5.65/webapps/ROOT.war /usr/local/lib/apache-tomcat-8.5.65/backup/ROOT_${today}.war
+echo "> backup sucess"
