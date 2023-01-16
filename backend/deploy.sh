4e3b8ace2881715de9155c8ed9fbb256aba1fd39
RougeOnCheeks <liliana@kakao.com>
2023-01-16 11:47:58 +0900

    deploy.sh - backup today format 변경 backend.yml - jar 파일만 zip 압축 frontend.yml - 수정 업데이트 backend push main #14

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "> BE 배포"
-today=$(date "+%Y-%m-%d %H:%M")
+today=$(date "+%Y%m%d%H%M")
 sudo cp /usr/local/lib/apache-tomcat-8.5.65/webapps/ROOT.war /usr/local/lib/apache-tomcat-8.5.65/backup/ROOT_${today}.war
-echo "> backup sucess"
+echo "> backup success"
