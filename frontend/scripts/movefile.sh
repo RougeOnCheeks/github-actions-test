3762209f85ad1fd210ac1b807a00a276e67aaf6e
RougeOnCheeks <liliana@kakao.com>
2023-01-16 20:18:10 +0900

    frontend push main #2

--- frontend/scripts/movefile.sh
+++ frontend/scripts/movefile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "> FE deploy"
 # 파일 이름 변경 및 root 폴더로 이동 -> autoDeploy
-sudo mv /var/www/static /var/www/backup/static_test
-sudo mv /var/www/index.html /var/www/backup/index_test.html
+sudo mv /var/www/config/dist/static /var/www/backup/static_test
+sudo mv /var/www/config/dist/index.html /var/www/backup/index_test.html
 echo "> deploy start"
