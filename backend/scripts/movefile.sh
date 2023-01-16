bbd8fd118d7f7214be29477690d1ebaf014ab878
RougeOnCheeks <liliana@kakao.com>
2023-01-16 15:29:12 +0900

    backend push main #18 appspec.yml - upload directory 변경, source 원복 backend.yml - create zip 원복 movefile.sh - 경로 변경

--- backend/scripts/movefile.sh
+++ backend/scripts/movefile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "> BE deploy"
 # 파일 이름 변경 및 tomcat root context 설정한 폴더로 이동 -> autoDeploy
-sudo mv map-study-0.0.1-SNAPSHOT.jar ../backup/map-study.jar #test
+sudo mv build/lib/map-study-0.0.1-SNAPSHOT.jar ../backup/map-study.jar #test
 echo "> deploy start"
