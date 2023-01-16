e9f510b286316a1c4616300b3679a390394b54eb
RougeOnCheeks <liliana@kakao.com>
2023-01-16 15:34:21 +0900

    backend push main #19 movefile.sh 오타 수정, 상대경로 수정

--- backend/scripts/movefile.sh
+++ backend/scripts/movefile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "> BE deploy"
 # 파일 이름 변경 및 tomcat root context 설정한 폴더로 이동 -> autoDeploy
-sudo mv build/lib/map-study-0.0.1-SNAPSHOT.jar ../backup/map-study.jar #test
+sudo mv ../build/libs/map-study-0.0.1-SNAPSHOT.jar /usr/local/lib/apache-tomcat-8.5.65/backup/map-study.jar #test
 echo "> deploy start"
