43824fd1848c857654e67c6d855453faf17bcf3b
RougeOnCheeks <liliana@kakao.com>
2023-01-18 16:22:55 +0900

    frontend push main #8 BeforeInstall fail -> cp 옵션 설정 변경 후 재시도

--- frontend/scripts/backup.sh
+++ frontend/scripts/backup.sh
@@ -3,5 +3,6 @@ echo "> FE 배포 전 backup"
 # 기존 파일 오늘 날짜 폴더에 복사
 TODAY=$(date "+%Y%m%d%H%M")
 sudo mkdir /var/www/backup/${TODAY}
-sudo cp -f /var/www/html /var/www/backup/${TODAY}
+sudo cp -r /var/www/html/static /var/www/backup/${TODAY}
+sudo cp /var/www/html/index.html /var/www/backup/${TODAY}
 echo "> backup success"
